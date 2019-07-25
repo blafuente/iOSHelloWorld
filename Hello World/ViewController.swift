e874d42a6b10de666a6a4fd7d9642cbf22ff3013
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-07-24 23:27:34 -0400

    first commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Brian Lafuente on 7/22/19.
+//  Copyright © 2019 Brian Lafuente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
